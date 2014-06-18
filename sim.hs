94f0a944551d6d1578f4d6ca815df080e46af9de
David Zmick <dpzmick@gmail.com>
2014-06-17 23:27:49 -0500

    added selflessness, I think

--- sim.hs
+++ sim.hs
@@ -1,11 +1,10 @@
-import Data.List (sortBy, maximumBy)
-import Data.Function (on)
 import System.Random.Shuffle (shuffle')
-import System.Random (getStdGen, RandomGen)
+import System.Random (getStdGen, RandomGen, randomRs)
 
 data Params = Params {
     memberContrib   :: Double,
     memberDetriment :: Double,
+    selflessness    :: Double,
     numJoiners      :: Integer,
     numGroups       :: Integer
 }
@@ -25,29 +24,51 @@ emptyGroups :: [Group]
 emptyGroups = [0,0..] :: [Group]
 
 fitness :: Params -> Group -> Double
-fitness (Params a b _ _) g =
+fitness (Params a b _ _ _) g =
         a * fromIntegral (size g) - (b / 2) * ( fromIntegral (size g) ^ (2 :: Integer) )
 
-
 -- sim stuff
-sim :: RandomGen g => Params -> g -> [Group]
-sim ps@(Params _ _ joiners groups) g =
-        iterate (oneJoin ps g) startingGroups !! fromIntegral joiners
+sim :: RandomGen g => Params -> g -> [Double] -> [Group]
+sim ps gen chances =
+        simIterate ps gen chances startingGroups (fromIntegral (numJoiners ps))
     where
-        startingGroups = take (fromIntegral groups) emptyGroups
+        startingGroups = take (fromIntegral (numGroups ps)) emptyGroups
+
+simIterate :: RandomGen g => Params -> g -> [Double] -> [Group] -> Integer -> [Group]
+simIterate ps gen chances gs remain
+    | remain /= 1 = simIterate ps gen (tail chances) (oneJoin ps gen chances gs) (remain - 1)
+    | remain == 1 = oneJoin ps gen chances gs
+    | otherwise = error "remain should never drop below 1"
 
 -- find all groups with best fitness, randomly pick one and join it
-oneJoin :: RandomGen g => Params -> g -> [Group] -> [Group]
-oneJoin ps gen gs =
+oneJoin :: RandomGen g => Params -> g -> [Double] -> [Group] -> [Group]
+oneJoin ps@(Params _ _ s _ _) gen (chance:_) gs =
+        if chance <= s
+            then selflessJoin ps gen gs
+            else normalJoin ps gen gs
+oneJoin _ _ [] _ = error "this list should never be empty"
+
+normalJoin :: RandomGen g => Params -> g -> [Group] -> [Group]
+normalJoin ps gen gs =
         addMember (head bestsShuffled) : tail bestsShuffled ++ rests
     where
         bestFitness = maximum (map (fitness ps) gs)
-        bests = filter (\ g -> (fitness ps g) == bestFitness) gs
-        rests = filter (\ g -> (fitness ps g) /= bestFitness) gs
+        bests = filter (\ g -> fitness ps g == bestFitness) gs
+        rests = filter (\ g -> fitness ps g /= bestFitness) gs
         bestsShuffled = shuffle' bests (length bests) gen
 
+selflessJoin :: RandomGen g => Params -> g -> [Group] -> [Group]
+selflessJoin ps gen gs =
+        case undamaged of
+            [ ] -> normalJoin ps gen gs -- if there are none, do a normal join
+            _   -> addMember (head undamaged) : tail undamaged ++ damaged
+        where
+            undamagedOrdered = filter (\ g -> fitness ps (addMember g) >= fitness ps g) gs
+            damaged          = filter (\ g -> fitness ps (addMember g) <  fitness ps g) gs
+            undamaged = shuffle' undamagedOrdered (length undamagedOrdered) gen
 
 main :: IO ()
 main = do
-        g <- getStdGen
-        print (sim (Params 1 0.5 5 10) g)
+        shuffleGen <- getStdGen
+        let chances = randomRs (0.0,1.0) shuffleGen :: [Double]
+        print (sim (Params 1 0.5 0.5 5 10) shuffleGen chances)
